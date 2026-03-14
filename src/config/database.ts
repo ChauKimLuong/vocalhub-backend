0536d933361fa646a50a1a1a967763e0a10e171d
ChauKimLuong <jinjun.116.05@gmail.com>
2026-03-14 22:48:10 +0700

    feat: configure database connect use singleton

---
+++ src/config/database.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+class Database {
+    private static instance: Database;
+    private readonly mongoUrl: string = process.env.MONGO_URI || "";
+
+    private constructor(){
+        this.connect();
+    }
+
+    public static getInstance(): Database{
+        if (!Database.instance){
+            Database.instance = new Database();
+       }
+       return Database.instance;
+    }
+
+    private async connect(): Promise<void> {
+        try {
+            await mongoose.connect(this.mongoUrl);
+            console.log("=====MongoDB connected successfully to VocalHub");
+        } catch (error) {
+            console.error("MongoDB connection error: ", error)
+            process.exit(1);
+        }
+    }
+
+}
+
+export default Database;
